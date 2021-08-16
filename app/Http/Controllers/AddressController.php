669e8fe96cbb2dd473fca5c9a887cd84e9d88fba
Maulana Krisna Adhitya <maulanakrisna@MoneyMakerPro.local>
2021-08-16 16:45:51 +0800

    mod route, most of controller

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -7,4 +7,22 @@ use Illuminate\Http\Request;
 class AddressController extends Controller
 {
     //
+    public function index(){
+      //
+    }
+    public function create(){
+      //
+    }
+    public function store(){
+      //
+    }
+    public function show(){
+      //
+    }
+    public function edit(){
+      //
+    }
+    public function update(){
+
+    }
 }
--- app/Http/Controllers/StiController.php
+++ app/Http/Controllers/StiController.php
@@ -7,4 +7,22 @@ use Illuminate\Http\Request;
 class StiController extends Controller
 {
     //
+    public function index(){
+      //
+    }
+    public function create(){
+      //
+    }
+    public function store(){
+      //
+    }
+    public function show(){
+      //
+    }
+    public function edit(){
+      //
+    }
+    public function update(){
+
+    }
 }
